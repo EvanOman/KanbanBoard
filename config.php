8f7729d94b77b663e650d1bc1543ba9a85dcde58
Evan Oman <evan.oman@blc.edu>
2012-09-07 16:37:00 -0500

    removed extra paths to bugzilla

--- config.php
+++ config.php
@@ -1,13 +1,3 @@
 <?php
-$source = 1 ;
- 
-if ($source == 0) {
-    define("BUGZILLA_URL", "http://software-pc/Bugzilla4.2/xmlrpc.cgi");    
-} 
-else if ($source == 1) {
-    define("BUGZILLA_URL", "http://software-pc/Bugzilla4.2(LambdaResearch)/xmlrpc.cgi");  
-}
-else  {
-    define("BUGZILLA_URL", "https://landfill.bugzilla.org/bugzilla-4.2-branch/xmlrpc.cgi");
-}
+define("BUGZILLA_URL", "https://landfill.bugzilla.org/bugzilla-4.2-branch/xmlrpc.cgi");
 ?>
